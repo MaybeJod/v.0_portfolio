f5fa1d0dd87ea39f047cc5d9cd86086308ca3c92
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-11-18 17:43:00 +0100

    feat: added contact secttion with local time

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import "./styles/reset.css";
 import "./styles/style.css";
 import tempJod from "./assets/img/temp_jod.jpg"
 import favIcon from "./assets/favicon/jod_favicon_6464.jpg"
+
+function getCurrentTime() {
+	const date = new Date();
+	const isoString = date.toISOString();
+	const formattedTime = isoString.split("T")[1].split(".")[0];
+	return formattedTime;
+}
+
+function updateTime() {
+	const currentTimeDisplay = document.querySelector("#currentTime");
+	currentTimeDisplay.innerText = getCurrentTime();
+	return currentTimeDisplay;
+}
+
+setInterval(updateTime, 1000);
