0ae110518ff2a88508ee07d688fb368df2d3d1f9
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-12-17 18:41:04 +0100

    fix: projects array now have more info, the projects card display how many projects there are

--- src/assets/components/projects.js
+++ src/assets/components/projects.js
@@ -4,61 +4,55 @@ const project = (title, tag, image, link) => {
 
 const projects = [
 	project(
-		"Project 1",
-		["Web Development", "Web Design", "SEO"],
+		"V.0 Portfolio",
+		["Web Development", "Web Design"],
 		"https://images.unsplash.com/photo-1730121108301-65fe6cefb48d?q=80&w=3263&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		"https://github.com/MaybeJod"
+		"https://github.com/MaybeJod/v.0_portfolio"
 	),
 	project(
-		"Project 2",
-		["Web Development", "Web Design", "SEO"],
+		"E-store",
+		["Web Development", "SEO"],
 		"https://images.unsplash.com/photo-1730121108301-65fe6cefb48d?q=80&w=3263&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		"https://github.com/MaybeJod"
+		"https://github.com/MaybeJod/e-store"
 	),
 	project(
-		"Project 3",
-		["Web Development", "Web Design", "SEO"],
+		"Weather app",
+		["Web Development"],
 		"https://images.unsplash.com/photo-1730121108301-65fe6cefb48d?q=80&w=3263&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		"https://github.com/MaybeJod"
+		"https://github.com/MaybeJod/ODIN-weather-app"
 	),
-	project(
-		"Project 4",
-		["Web Development", "Web Design", "SEO"],
-		"https://images.unsplash.com/photo-1730121108301-65fe6cefb48d?q=80&w=3263&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		"https://github.com/MaybeJod"
-	)
 ];
 
 export default function renderProjects() {
-    const projectsSection = document.querySelector("#projects");
-    const gridPageContainer = document.createElement("div");
-    const projectsSectionTitle = document.createElement("h1");
-    const gridContainer = document.createElement("div");
+	const projectsSection = document.querySelector("#projects");
+	const gridPageContainer = document.createElement("div");
+	const projectsSectionTitle = document.createElement("h1");
+	const gridContainer = document.createElement("div");
 
-    gridPageContainer.classList.add("page-container");
-    projectsSectionTitle.classList.add("projects-heading");
-    projectsSectionTitle.textContent = "Creations in progress";
+	gridPageContainer.classList.add("page-container");
+	projectsSectionTitle.classList.add("projects-heading");
+	projectsSectionTitle.textContent = "Creations in progress";
 
-    gridContainer.classList.add("grid-container");
-    
-    projectsSection.appendChild(gridPageContainer);
-    gridPageContainer.appendChild(projectsSectionTitle);
-    gridPageContainer.appendChild(gridContainer);
+	gridContainer.classList.add("grid-container");
 
-    projects.forEach((project) => {
-        const card = document.createElement("article")
-        card.classList.add("card");
+	projectsSection.appendChild(gridPageContainer);
+	gridPageContainer.appendChild(projectsSectionTitle);
+	gridPageContainer.appendChild(gridContainer);
 
-        card.innerHTML = `
+	projects.forEach((project, index) => {
+		const card = document.createElement("article");
+		card.classList.add("card");
+
+		card.innerHTML = `
             <a href="${project.link}">
-                <span class="project-number">00-1</span>
+                <span class="project-number">00-${index + 1}</span>
                 <img src="${project.image}">
                 <hr>
                 <p class="project-tags">${project.tag.join(" - ")}</p>
                 <h1 class="project-title">${project.title}</h1>
             </a>
-        `
+        `;
 
-        gridContainer.appendChild(card);
-    })
+		gridContainer.appendChild(card);
+	});
 }
